4f39c59c23c6a26fec357cae61685d2415481102
jsl <jslim@smu.ac.kr>
2024-04-05 12:08:49 +0900

    wk5

--- src/_gethNow.bat
+++ src/_gethNow.bat
@@ -1,1 +1,1 @@
-geth --identity "jsl" --allow-insecure-unlock --unlock 0 --datadir .\eth --ws --ws.port 8446 --ws.origins "*" --httpp --http.addr "localhost" --http.port "8445" --http.corsdomain "*" --http.api "admin, db, eth, debug, miner, net, shh, txpool, personal, web3" --port "38445" --networkid 33
+geth --identity "jsl" --allow-insecure-unlock --unlock 0 --datadir .\eth --ws --ws.port 8446 --ws.origins "*" --http --http.addr "localhost" --http.port "8445" --http.corsdomain "*" --http.api "admin, db, eth, debug, miner, net, shh, txpool, personal, web3" --port "38445" --networkid 33
